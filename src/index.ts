dd6e823cbc5ce2c5d38b3dfc2dce4be9eb1e2db4
Lalatendu <lalatendupany.2000@gmail.com>
2025-01-14 00:01:30 +0530

    changes file name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import connectToDatabase from "./utils/databaseconnection";
+import connectToDatabase from "./utils/databaseConnection";
 import routes from "./routes/app";
 dotenv.config();
 const app = express();
